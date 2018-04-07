4e1717e9cfcac074eaac6481762e9c4c2a60d6df
大神琢也 <oogamitakuya@oogamitakuyanoMacBook-Pro.local>
2018-04-07 10:39:46 +0900

    Development.rbの編集

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-protospace_in_dev_master::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
