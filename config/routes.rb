b13969ce1ebabb425b4c5a8dc89abc8ac9855113
toru tateyama <feet-10@hotmail.co.jp>
2018-04-09 10:17:26 +0900

    like機能の設定　ルーdティング　ネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :prototypes
+  end
   resources :prototypes, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
 end
