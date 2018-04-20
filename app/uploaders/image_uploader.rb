c740d3182dd25a6053066f3ce639c7e3cca81599
fusemawari <fusemawari@gmail.com>
2018-04-20 11:27:26 +0900

    fix_carrierwave
    一旦コメントアウトにしています

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,21 +1,21 @@
-class ImageUploader < CarrierWave::Uploader::Base
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+# class ImageUploader < CarrierWave::Uploader::Base
+#   if Rails.env.development?
+#     storage :file
+#   elsif Rails.env.test?
+#     storage :file
+#   else
+#     storage :fog
+#   end
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+#   def store_dir
+#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#   end
 
-  def extension_whitelist
-    %w(png jpg)
-  end
+#   def extension_whitelist
+#     %w(png jpg)
+#   end
 
-  def filename
-    original_filename if original_filename
-  end
-end
+#   def filename
+#     original_filename if original_filename
+#   end
+# end
